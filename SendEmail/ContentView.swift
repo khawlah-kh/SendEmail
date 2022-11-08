30d2129caae8972d29ab94e8aa675475f84e1db8
Khawlah Khalid <khawlah1920@hotmail.com>
2022-11-08 17:16:33 +0300

    Initial Commit

---
+++ SendEmail/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SendEmail
+//
+//  Created by Khawlah Khalid on 08/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SendEmail/SendEmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendEmailApp.swift
+//  SendEmail
+//
+//  Created by Khawlah Khalid on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SendEmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
